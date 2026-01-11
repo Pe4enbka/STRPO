6dd5fdde9943e12d3c01ab9b27c7fb9fe554bb4b
Pe4enbka <72940882+Pe4enbka@users.noreply.github.com>
2026-01-11 21:11:35 +0400

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,10 +2,6 @@
 
 int main() {
     std::string name;  // Имя пользователя
-<<<<<<< HEAD
->>>>>>> a063cd5 (Patch2: Code formatted and comments added)
-=======
->>>>>>> a063cd59b3f06da78094ab15ff45ccff129d087a
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from " << name;  // Вывод приветствия
