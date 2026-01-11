ce444b80ed946707ddcb833ddee38f10a6a12c81
Dany <sstudanyst@gmail.com>
2026-01-11 20:52:57 +0400

    Patch2: Code formatted and comments added

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
-// Программа выводит приветствие пользователю
-int main()
-{
-    std::string name; // переменная побита
+int main() {
+    std::string name;  // Имя пользователя
+>>>>>>> a063cd5 (Patch2: Code formatted and comments added)
     std::cout << "Enter your name: ";
     std::cin >> name;
-    std::cout << "Hello world from " << name; // выводим приветствие
+    std::cout << "Hello world from " << name;  // Вывод приветствия
     return 0;
 }
